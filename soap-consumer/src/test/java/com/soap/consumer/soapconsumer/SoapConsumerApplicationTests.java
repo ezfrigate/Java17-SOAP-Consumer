0981072c493e33fa61a0e2622538b62c1ddd2023
Swapnil Bole <Swapnil.Bole@se.com>
2023-03-22 20:06:36 +0530

    Setting up SOAP client

---
+++ soap-consumer/src/test/java/com/soap/consumer/soapconsumer/SoapConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.consumer.soapconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
