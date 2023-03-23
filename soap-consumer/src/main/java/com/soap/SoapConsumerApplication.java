8f0e3fb3c6bdbc3f77fd9138b43f67737c5bfb88
ezfrigate <swapnilbole119@gmail.com>
2023-03-23 12:22:22 +0530

    Pom file configuration

--- soap-consumer/src/main/java/com/soap/SoapConsumerApplication.java
+++ soap-consumer/src/main/java/com/soap/SoapConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.soap.consumer.soapconsumer;
+package com.soap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
